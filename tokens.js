048d8fd8a1d6aa72d68d3cb2ce3fbe2660164aef
poplima <efstice7@gmail.com>
2025-07-06 01:33:23 +0100

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -14,19 +14,4 @@ export default [
         },
         selfMute: false,
     },
-    {
-        channelId: "1389483516193935410",
-        serverId: "777271906486976512",
-        token: process.env.sexy,
-        selfDeaf: false,
-        autoReconnect: {
-            enabled: true,
-            delay: 15,
-            maxRetries: 50,
-        },
-        presence: {
-            status: "dnd",
-        },
-        selfMute: true,
-    },
-];
+]
